a6e8cd7344c66089a7c701102cbb3e343bcb3fc3
cally2k <callum.rafter@gmail.com>
2020-11-07 11:25:53 +0000

    V1.0.3

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {res.send(database.users)})
+app.get('/', (req, res) => {res.send('Working')})
 
 app.post("/signin", signin.handleSigninPost(db, bcrypt))
 
